1a92a5a72906170a0a0d058fdbcdc6be68e0cbe8
Shria Banerjee <54706686+BShria@users.noreply.github.com>
2022-02-02 21:35:27 +0530

    Day 2 Submmitted

---
+++ Day2.py
@@ -0,0 +1,28 @@
+# find the character with the maximum frequency from the input string
+
+s = input("Enter the lottery string: ")
+
+# using list
+freqList = [0] * 26
+
+for i in s:
+  freqList[(ord(i) - 97)] += 1
+
+ans = freqList.index(max(freqList))
+print(chr(ans + 97)) 
+# chr function converts integer to character
+
+# using dictionary
+freqDict = {}
+
+for i in s:
+  freqDict[i] = freqDict.get(i,0) + 1
+
+maxFreq = 0
+ansChar = '$'
+for i in freqDict.items():
+  if(i[1] > maxFreq):
+    maxFreq = i[1]
+    ansChar = i[0]
+
+print(ansChar)
